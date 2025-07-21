90155cd9714c6faeecd6147db311fcdab604fe86
RuthikaMitta <mittaruthika@gmail.com>
2025-07-21 12:06:09 +0530

    Your commit message

--- profile/script.js
+++ profile/script.js
@@ -23,3 +23,25 @@ document.getElementById("profileForm").addEventListener("submit", function (e) {
     document.getElementById("status").innerText = "Error saving profile!";
   });
 });
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABsASS7qrOTcDYHjxZ0jpp_P-n7zWt2QQ",
+  authDomain: "breasthealthweb.firebaseapp.com",
+  databaseURL: "https://breasthealthweb-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "breasthealthweb",
+  storageBucket: "breasthealthweb.firebasestorage.app",
+  messagingSenderId: "147361987150",
+  appId: "1:147361987150:web:48c8285647f7b681d8211f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+// Display live temperature
+const tempRef = ref(db, 'sensors/temperature');
+onValue(tempRef, (snapshot) => {
+  const temp = snapshot.val();
+  document.getElementById('live-temp').innerText = `Temperature: ${temp} °C`;
+});
